86b94a669aee25158372755ee3dd1c2429b40eeb
Andrea Cardamone <akira80@gmail.com>
2019-01-18 16:10:15 +0100

    test with curl

--- js/andrea.js
+++ js/andrea.js
@@ -2,6 +2,9 @@ var _ = require("../lib/_node.js");
 var S = require('string');
 var request = require("request");
 var fs = require('fs');
+const http = require('https')
+const util = require('util');
+const exec = util.promisify(require('child_process').exec); 
 
 var jsdom = require("jsdom");
 const { JSDOM } = jsdom;
@@ -11,13 +14,24 @@ global.document = document;
 
 var $ = jQuery = require('jquery')(window);
 
+async function getDOM(url) {
+  return (await exec(url)).stdout
+}
 
+async function main() {
+  let res = await getDOM('https://portal.vistosi.it/eprogen/epRichiesta_risorse_pubblica_v2.jsp?cdvisttp=SP&cdvistfam=ASSIB&cdling=0&fg_eur_usa=console.error')
+    
+  console.log(res)
+}
 
-
-request({
-        uri: encodeURI("https://portal.vistosi.it/eprogen/epRichiesta_risorse_pubblica_v2.jsp?cdvisttp=SP&cdvistfam=ASSIB&cdling=0&fg_eur_usa=E"),
-        
-
-    }, function(error, response, body) {
-        _.log(body);
-    });
+if (!!require.main) {
+  (async () => {
+    try {
+      await main()
+    } catch (e) {
+      console.error(e);
+    } finally {
+      /* release resouce as db connection */
+    }
+  })()
+}
